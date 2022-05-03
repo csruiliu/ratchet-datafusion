33e2a1e9a4577b28950bb03928f8e652b7d9cd63
csruiliu <rui.liu@gmx.com>
2022-05-03 16:52:13 -0500

    update client-server

--- src/common/src/commands.rs
+++ src/common/src/commands.rs
@@ -1,12 +1,11 @@
+use serde::{Serialize, Deserialize};
 
 pub enum Commands {
     // it is a enum member with tuple
     /// create a database
     CreateDB(String),
     /// Connect to a database
-    ConnectDB(String),
-    /// Import a database
-    ImportDB(String)
+    ConnectDB(String)
 }
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
@@ -14,4 +13,27 @@ pub enum Response {
     Ok,
     Msg(String),
     Err(String)
+}
+
+pub fn parse_command(mut cmd: String) -> Option<Commands> {
+    if cmd.ends_with('\n') {
+        // remove the last character '\n'
+        cmd.pop();
+        if cmd.ends_with('\r') {
+            cmd.pop();
+        }
+    }
+    if cmd.ends_with('\r') {
+        cmd.pop();
+    }
+
+    if cmd.starts_with("create ") {
+        return Some(Commands::CreateDB(cmd[7..].to_string()));
+    }
+    else if cmd.starts_with("connect ") {
+        return Some(Commands::ConnectDB(cmd[8..].to_string()));
+    }
+    else {
+        return None;
+    }
 }
