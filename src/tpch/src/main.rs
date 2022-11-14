d6d1b6541d20704634a65edca30f0e37744b33e5
csruiliu <rui.liu@gmx.com>
2022-11-14 11:24:36 -0600

    add main and cargo toml

---
+++ src/tpch/src/main.rs
@@ -0,0 +1,430 @@
+
+use std::{
+    fs,
+    fs::File,
+    sync::Arc,
+    path::{Path, PathBuf},
+    time::{Instant, SystemTime}
+};
+
+use structopt::StructOpt;
+
+use datafusion::arrow::{
+    record_batch::RecordBatch,
+    datatypes::DataType,
+    datatypes::Field,
+    datatypes::Schema
+};
+
+use datafusion::datasource::{
+    MemTable,
+    TableProvider,
+    listing::{ListingOptions, ListingTable, ListingTableConfig, ListingTableUrl},
+    file_format::parquet::{ParquetFormat, DEFAULT_PARQUET_EXTENSION},
+    file_format::csv::{CsvFormat, DEFAULT_CSV_EXTENSION},
+    file_format::FileFormat
+};
+
+use datafusion::physical_plan::{
+    display::DisplayableExecutionPlan,
+    collect,
+    displayable
+};
+
+use datafusion::execution::{
+    context::SessionState
+};
+
+use datafusion::{
+    error::{DataFusionError, Result},
+    prelude::*
+};
+
+const TPCH_QUERY_START_ID: usize = 1;
+const TPCH_QUERY_END_ID: usize = 22;
+const TPCH_TABLES: &[&str] = &[
+    "part", "supplier", "partsupp", "customer", "orders", "lineitem", "nation", "region",
+];
+
+#[derive(Debug, StructOpt, Clone)]
+struct DataFusionBenchmarkOpt {
+    /// Query number. If not specified, runs all queries
+    #[structopt(short, long)]
+    query: Option<usize>,
+
+    /// Activate debug mode to see query results
+    #[structopt(short, long)]
+    debug: bool,
+
+    /// Number of iterations of each test run
+    #[structopt(short = "i", long = "iterations", default_value = "1")]
+    iterations: usize,
+
+    /// Number of partitions to process in parallel
+    #[structopt(short = "n", long = "partitions", default_value = "1")]
+    partitions: usize,
+
+    /// Batch size when reading CSV or Parquet files
+    #[structopt(short = "s", long = "batch-size", default_value = "8192")]
+    batch_size: usize,
+
+    /// Path to data files
+    #[structopt(parse(from_os_str), required = true, short = "p", long = "path")]
+    path: PathBuf,
+
+    /// File format: `csv` or `parquet`
+    #[structopt(short = "f", long = "format", default_value = "tbl")]
+    file_format: String,
+
+    /// Load the data into a MemTable before executing the query
+    #[structopt(short = "m", long = "mem-table")]
+    mem_table: bool,
+
+    /// Path to output directory where JSON summary file should be written to
+    #[structopt(parse(from_os_str), short = "o", long = "output")]
+    output_path: Option<PathBuf>,
+
+    /// Whether to disable collection of statistics (and cost based optimizations) or not.
+    #[structopt(short = "S", long = "disable-statistics")]
+    disable_statistics: bool,
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "TPC-H", about = "TPC-H Benchmarks.")]
+enum TpchOpt {
+    Benchmark(DataFusionBenchmarkOpt)
+}
+
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    match TpchOpt::from_args() {
+        TpchOpt::Benchmark(opt) => {
+            benchmark_datafusion(opt).await.map(|_| ())
+        }
+    }
+}
+
+async fn benchmark_datafusion(
+    opt: DataFusionBenchmarkOpt,
+) -> Result<Vec<Vec<RecordBatch>>> {
+    // println!("Running benchmarks with the following options: {:?}", opt);
+    let query_range = match opt.query {
+        Some(query_id) => query_id..=query_id,
+        None => TPCH_QUERY_START_ID..=TPCH_QUERY_END_ID,
+    };
+
+    let mut results = vec![];
+    for query_id in query_range {
+        let result = benchmark_query(&opt, query_id).await?;
+        results.push(result);
+    }
+
+    Ok(results)
+}
+
+async fn benchmark_query(
+    opt: &DataFusionBenchmarkOpt,
+    query_id: usize,
+) -> Result<Vec<RecordBatch>> {
+    let config = SessionConfig::new()
+        .with_target_partitions(opt.partitions)
+        .with_batch_size(opt.batch_size)
+        .with_collect_statistics(!opt.disable_statistics);
+
+    let ctx = SessionContext::with_config(config);
+
+    // register tables
+    register_tables(opt, &ctx).await?;
+
+    let mut millis = vec![];
+    // run benchmark
+    let mut result: Vec<RecordBatch> = Vec::with_capacity(1);
+    for i in 0..opt.iterations {
+        let start = Instant::now();
+
+        let sql = &get_query_sql(query_id)?;
+
+        // query 15 is special, with 3 statements. the second statement is the one from which we
+        // want to capture the results
+        if query_id == 15 {
+            for (n, query) in sql.iter().enumerate() {
+                if n == 1 {
+                    result = execute_query(&ctx, query, opt.debug).await?;
+                } else {
+                    execute_query(&ctx, query, opt.debug).await?;
+                }
+            }
+        } else {
+            for query in sql {
+                result = execute_query(&ctx, query, opt.debug).await?;
+            }
+        }
+
+        let elapsed = start.elapsed().as_secs_f64() * 1000.0;
+        millis.push(elapsed as f64);
+        let row_count: usize = result.iter().map(|b| b.num_rows()).sum();
+        println!(
+            "Query {} iteration {} took {:.1} ms and returned {} rows",
+            query_id, i, elapsed, row_count
+        );
+    }
+
+    let avg = millis.iter().sum::<f64>() / millis.len() as f64;
+    println!("Query {} avg time: {:.2} ms", query_id, avg);
+
+    Ok(result)
+}
+
+#[allow(clippy::await_holding_lock)]
+async fn register_tables(
+    opt: &DataFusionBenchmarkOpt,
+    ctx: &SessionContext,
+) -> Result<()> {
+    for table in TPCH_TABLES {
+        let table_provider = {
+            let mut session_state = ctx.state.write();
+            get_table(
+                &mut session_state,
+                opt.path.to_str().unwrap(),
+                table,
+                opt.file_format.as_str(),
+                opt.partitions,
+            ).await?
+        };
+
+        if opt.mem_table {
+            println!("Loading table '{}' into memory", table);
+            let start = Instant::now();
+            let memtable = MemTable::load(
+                table_provider,
+                Some(opt.partitions),
+                &ctx.state()
+            ).await?;
+            println!(
+                "Loaded table '{}' into memory in {} ms",
+                table,
+                start.elapsed().as_millis()
+            );
+            ctx.register_table(*table, Arc::new(memtable))?;
+        } else {
+            ctx.register_table(*table, table_provider)?;
+        }
+    }
+    Ok(())
+}
+
+async fn get_table(
+    ctx: &mut SessionState,
+    path: &str,
+    table: &str,
+    table_format: &str,
+    target_partitions: usize,
+) -> Result<Arc<dyn TableProvider>> {
+    let (format, path, extension): (Arc<dyn FileFormat>, String, &'static str) =
+        match table_format {
+            // dbgen creates .tbl ('|' delimited) files without header
+            "tbl" => {
+                let path = format!("{}/{}.tbl", path, table);
+
+                let format = CsvFormat::default()
+                    .with_delimiter(b'|')
+                    .with_has_header(false);
+
+                (Arc::new(format), path, ".tbl")
+            }
+            "csv" => {
+                let path = format!("{}/{}", path, table);
+                let format = CsvFormat::default()
+                    .with_delimiter(b',')
+                    .with_has_header(true);
+
+                (Arc::new(format), path, DEFAULT_CSV_EXTENSION)
+            }
+            "parquet" => {
+                let path = format!("{}/{}", path, table);
+                let format = ParquetFormat::default().with_enable_pruning(true);
+
+                (Arc::new(format), path, DEFAULT_PARQUET_EXTENSION)
+            }
+            other => {
+                unimplemented!("Invalid file format '{}'", other);
+            }
+        };
+    let schema = Arc::new(get_tpch_table_schema(table));
+
+    let options = ListingOptions {
+        format,
+        file_extension: extension.to_owned(),
+        target_partitions,
+        collect_stat: ctx.config.collect_statistics,
+        table_partition_cols: vec![],
+    };
+
+    let table_path = ListingTableUrl::parse(path)?;
+    let config = ListingTableConfig::new(table_path).with_listing_options(options);
+
+    let config = if table_format == "parquet" {
+        config.infer_schema(ctx).await?
+    } else {
+        config.with_schema(schema)
+    };
+
+    Ok(Arc::new(ListingTable::try_new(config)?))
+}
+
+fn get_tpch_table_schema(table: &str) -> Schema {
+    // note that the schema intentionally uses signed integers so that any generated Parquet
+    // files can also be used to benchmark tools that only support signed integers, such as
+    // Apache Spark
+
+    match table {
+        "part" => Schema::new(vec![
+            Field::new("p_partkey", DataType::Int64, false),
+            Field::new("p_name", DataType::Utf8, false),
+            Field::new("p_mfgr", DataType::Utf8, false),
+            Field::new("p_brand", DataType::Utf8, false),
+            Field::new("p_type", DataType::Utf8, false),
+            Field::new("p_size", DataType::Int32, false),
+            Field::new("p_container", DataType::Utf8, false),
+            Field::new("p_retailprice", DataType::Decimal128(15, 2), false),
+            Field::new("p_comment", DataType::Utf8, false),
+        ]),
+
+        "supplier" => Schema::new(vec![
+            Field::new("s_suppkey", DataType::Int64, false),
+            Field::new("s_name", DataType::Utf8, false),
+            Field::new("s_address", DataType::Utf8, false),
+            Field::new("s_nationkey", DataType::Int64, false),
+            Field::new("s_phone", DataType::Utf8, false),
+            Field::new("s_acctbal", DataType::Decimal128(15, 2), false),
+            Field::new("s_comment", DataType::Utf8, false),
+        ]),
+
+        "partsupp" => Schema::new(vec![
+            Field::new("ps_partkey", DataType::Int64, false),
+            Field::new("ps_suppkey", DataType::Int64, false),
+            Field::new("ps_availqty", DataType::Int32, false),
+            Field::new("ps_supplycost", DataType::Decimal128(15, 2), false),
+            Field::new("ps_comment", DataType::Utf8, false),
+        ]),
+
+        "customer" => Schema::new(vec![
+            Field::new("c_custkey", DataType::Int64, false),
+            Field::new("c_name", DataType::Utf8, false),
+            Field::new("c_address", DataType::Utf8, false),
+            Field::new("c_nationkey", DataType::Int64, false),
+            Field::new("c_phone", DataType::Utf8, false),
+            Field::new("c_acctbal", DataType::Decimal128(15, 2), false),
+            Field::new("c_mktsegment", DataType::Utf8, false),
+            Field::new("c_comment", DataType::Utf8, false),
+        ]),
+
+        "orders" => Schema::new(vec![
+            Field::new("o_orderkey", DataType::Int64, false),
+            Field::new("o_custkey", DataType::Int64, false),
+            Field::new("o_orderstatus", DataType::Utf8, false),
+            Field::new("o_totalprice", DataType::Decimal128(15, 2), false),
+            Field::new("o_orderdate", DataType::Date32, false),
+            Field::new("o_orderpriority", DataType::Utf8, false),
+            Field::new("o_clerk", DataType::Utf8, false),
+            Field::new("o_shippriority", DataType::Int32, false),
+            Field::new("o_comment", DataType::Utf8, false),
+        ]),
+
+        "lineitem" => Schema::new(vec![
+            Field::new("l_orderkey", DataType::Int64, false),
+            Field::new("l_partkey", DataType::Int64, false),
+            Field::new("l_suppkey", DataType::Int64, false),
+            Field::new("l_linenumber", DataType::Int32, false),
+            Field::new("l_quantity", DataType::Decimal128(15, 2), false),
+            Field::new("l_extendedprice", DataType::Decimal128(15, 2), false),
+            Field::new("l_discount", DataType::Decimal128(15, 2), false),
+            Field::new("l_tax", DataType::Decimal128(15, 2), false),
+            Field::new("l_returnflag", DataType::Utf8, false),
+            Field::new("l_linestatus", DataType::Utf8, false),
+            Field::new("l_shipdate", DataType::Date32, false),
+            Field::new("l_commitdate", DataType::Date32, false),
+            Field::new("l_receiptdate", DataType::Date32, false),
+            Field::new("l_shipinstruct", DataType::Utf8, false),
+            Field::new("l_shipmode", DataType::Utf8, false),
+            Field::new("l_comment", DataType::Utf8, false),
+        ]),
+
+        "nation" => Schema::new(vec![
+            Field::new("n_nationkey", DataType::Int64, false),
+            Field::new("n_name", DataType::Utf8, false),
+            Field::new("n_regionkey", DataType::Int64, false),
+            Field::new("n_comment", DataType::Utf8, false),
+        ]),
+
+        "region" => Schema::new(vec![
+            Field::new("r_regionkey", DataType::Int64, false),
+            Field::new("r_name", DataType::Utf8, false),
+            Field::new("r_comment", DataType::Utf8, false),
+        ]),
+
+        _ => unimplemented!(),
+    }
+}
+
+fn get_query_sql(query: usize) -> Result<Vec<String>> {
+    if query > 0 && query < 23 {
+        let possibilities = vec![
+            format!("queries/q{}.sql", query),
+            format!("benchmarks/queries/q{}.sql", query),
+        ];
+        let mut errors = vec![];
+        for filename in possibilities {
+            match fs::read_to_string(&filename) {
+                Ok(contents) => {
+                    return Ok(contents
+                        .split(';')
+                        .map(|s| s.trim())
+                        .filter(|s| !s.is_empty())
+                        .map(|s| s.to_string())
+                        .collect());
+                }
+                Err(e) => errors.push(format!("{}: {}", filename, e)),
+            };
+        }
+        Err(DataFusionError::Plan(format!(
+            "invalid query. Could not find query: {:?}",
+            errors
+        )))
+    } else {
+        Err(DataFusionError::Plan(
+            "invalid query. Expected value between 1 and 22".to_owned(),
+        ))
+    }
+}
+
+async fn execute_query(
+    ctx: &SessionContext,
+    sql: &str,
+    debug: bool,
+) -> Result<Vec<RecordBatch>> {
+    let plan = ctx.sql(sql).await?;
+    let plan = plan.to_unoptimized_plan();
+
+    if debug {
+        println!("=== Logical plan ===\n{:?}\n", plan);
+    }
+
+    let plan = ctx.optimize(&plan)?;
+    if debug {
+        println!("=== Optimized logical plan ===\n{:?}\n", plan);
+    }
+    let physical_plan = ctx.create_physical_plan(&plan).await?;
+
+    if debug {
+        println!(
+            "=== Physical plan ===\n{}\n",
+            displayable(physical_plan.as_ref()).indent()
+        );
+    }
+    let task_ctx = ctx.task_ctx();
+    let result = collect(physical_plan.clone(), task_ctx).await?;
+
+    Ok(result)
+}
