4c68454b22f94b5b01c727a5bb2087190510d40c
csruiliu <rui.liu@gmx.com>
2022-12-02 13:48:13 -0600

    update criu installation script

--- src/tpch/criu_installation.sh
+++ src/tpch/criu_installation.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
 # install gcc and make packages
-sudo apt-get install build-essential
+sudo apt-get install -y build-essential
 
 # install protocol buffers
-sudo apt-get install libprotobuf-dev libprotobuf-c-dev protobuf-c-compiler protobuf-compiler python3-protobuf
+sudo apt-get install -y libprotobuf-dev libprotobuf-c-dev protobuf-c-compiler protobuf-compiler python3-protobuf
 
 # install other required libs
-sudo apt-get install libcap-dev libnl-3-dev libnet-dev
+sudo apt-get install -y libcap-dev libnl-3-dev libnet-dev
 
 # install pkg-config to check on build library dependencies
-sudo apt-get install pkg-config
+sudo apt-get install -y pkg-config
 
+cd /tank/local/ruiliu
+
+wget https://github.com/checkpoint-restore/criu/archive/refs/tags/v3.17.1.tar.gz
+
+tar -xvf v3.17.1.tar.gz
+
+cd /tank/local/ruiliu/criu-3.17.1
+
+make
